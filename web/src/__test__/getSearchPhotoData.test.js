566c4524395073cdc03892a9dc9c2f55ca1e1d14
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-06-18 03:26:28 +0100

    Add test for mock search data

---
+++ web/src/__test__/getSearchPhotoData.test.js
@@ -0,0 +1,15 @@
+import '@testing-library/jest-dom/extend-expect';
+import fetchedData from '../Mock/getSearchPhotoData';
+
+describe('test fetched data', () => {
+  it('test fetching photo title', async () => {
+    await fetchedData().then((data) => expect(data.data[0].title).toBe('Image 02_32_18_33'));
+  });
+  it('test fetched photo location', async () => {
+    await fetchedData().then((data) => expect(data.data[0].nasa_id).toBe('Stennis Space Center'));
+  });
+
+  it('test fetched photo description_508', async () => {
+    await fetchedData().then((data) => expect(data.data[1].description_508).toBe('liquid oxygen can be seen venting near the top of the installed core stage'));
+  });
+});
